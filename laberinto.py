f65229a0d11a0233852f045c97efd05f1c2ad8f2
javierdiezde <fdiezde@gmail.com>
2018-04-26 11:00:31 +0200

    Add files via upload

--- laberinto.py
+++ laberinto.py
@@ -11,30 +11,46 @@ perseo = turtle.Turtle()
 window = turtle.Screen()
 setup(850, 650,0,0)
 colormode (255)
+perseo.penup()
+perseo.goto(-200,-100)
+perseo.pendown()
 
 def ira(x,y):
     perseo.goto(x,y)
     print(x,y)
+    restricciones(x,y)
 def arriba():
     perseo.setheading(90)
     perseo.forward(10)
 def muere():
+    perseo.penup()
+    perseo.goto(-200,-199)
+    perseo.pendown()
+
     
 def restricciones(x,y):
-    # círculo central
-    if x^2+y^2<100:
+    # círculo central de radio 10
+    if x**2+y**2<100:
+        perseo.penup()
+        pereseo.goto(0,0)
+        perseo.pendown()
+        perseo.circle(10)
         muere()
+    elif x<2 and x>-100 and -10<y and y<10: # rectángulo -100<x<2 -10<y<10
+        perseo.penup()
+        perseo.goto (-100,-10)
+        perseo.pendown()
+        
+        
+        
+        
+    
         
     
 window.listen()
 
-while True:
-    window.onkeypress(arriba,'Up')
-    window.onclick(ira)
-   
+window.onkeypress(arriba,'Up')
+window.onclick(ira)
+
+
     
-        
-
-
-
-
